0b8d1945c47df813ce844f244b0e7f08f674848c
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-14 00:35:43 -0700

    Add files via upload

--- Delete Operation for Two Strings.py
+++ Delete Operation for Two Strings.py
@@ -19,3 +19,11 @@ class Solution:
                 else:
                     dp[r][c] = 1+min(dp[r-1][c],dp[r][c-1])
         return dp[-1][-1]
+
+        # Solution 2
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n + 1) for i in range(m + 1)]
+        for i in range(m):
+            for j in range(n):
+                dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j], dp[i][j] + (word1[i] == word2[j]))
+        return m + n - 2 * dp[m][n]
